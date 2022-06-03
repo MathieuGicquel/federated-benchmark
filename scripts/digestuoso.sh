8b28782da19789f2d0a08e9d9f8194ada0c42f99
Mathieu Gicquel <59269440+MathieuGicquel@users.noreply.github.com>
2022-06-03 11:24:32 +0200

    fix

--- scripts/digestuoso.sh
+++ scripts/digestuoso.sh
@@ -11,4 +11,5 @@ do
   string+="DELETE FROM rdf_quad WHERE g = iri_to_id('$line'); \n"
 done < $2
   echo -e "$string" \
-  | eval "/mnt/c/Program\\ Files/OpenLink\\ Software/Virtuoso\\ OpenSource\\ 7.2/bin/isql.exe 1111 dba dba" #  | eval $PARAM1 "localhost:1111 dba dba"
+  #| eval "/mnt/c/Program\\ Files/OpenLink\\ Software/Virtuoso\\ OpenSource\\ 7.2/bin/isql.exe 1111 dba dba"
+   | eval $PARAM1 "localhost:1111 dba dba"
