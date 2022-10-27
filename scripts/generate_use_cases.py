62c3e22d860841aa6722ea814f5c96dc703c4a1c
Mathieu Gicquel <59269440+MathieuGicquel@users.noreply.github.com>
2022-10-27 20:51:41 +0200

    add nodes threshold

--- scripts/generate_use_cases.py
+++ scripts/generate_use_cases.py
@@ -13,12 +13,14 @@ from lxml.html.soupparser import fromstring
 from lxml.etree import tostring
 import re
 
+
+MIN_NODES = 100000
 # Example of use :
 # python3 ./scripts/generate_use_cases.py ./use-case/shop.xml ./prepa/use-cases
 
 # Goal : Take a shop.xml file template and apply the data distribution from configuration.yaml on it to have a variation of number of nodes
 
-coloredlogs.install(level='DEBUG', fmt='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
+coloredlogs.install(level='INFO', fmt='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
 logger = logging.getLogger(__name__)
 
 configuration = yaml.load(open("configuration.yaml"), Loader=yaml.FullLoader)
@@ -58,8 +60,12 @@ def generate_use_cases(shop_template,output):
     logger.debug(new_values)
 
     for i, val in enumerate(new_values):
+        if(val < MIN_NODES):
+            logger.debug("%s < %s" % (val,MIN_NODES))
+            val = MIN_NODES
+            
         tag.string.replace_with(str(int(val)))
-        logger.debug(str(tag.string))
+        logger.info(f'{output}/shop-{i}.xml has ' + str(tag.string) + " nodes")
         with open(f'{output}/shop-{i}.xml','w') as outfile:
             str_xml = str(bs_data.prettify())
             str_xml = re.sub(r"(\s*<.+>)\n\s*([A-Za-z0-9\.]+)\n\s*(</.+>)\n",r"\1\2\3\n",str_xml)
