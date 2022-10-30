b4f8e48a661de465f018f14ae8e41bfa6289dcac
Mathieu Gicquel <59269440+MathieuGicquel@users.noreply.github.com>
2022-10-30 02:30:05 +0200

    rewrite code using big data tools

--- scripts/compute_statistics.py
+++ scripts/compute_statistics.py
@@ -5,96 +5,111 @@ import warnings
 from collections import defaultdict
 import click
 import coloredlogs
-import pandas as pd
 import yaml
+from pyspark.sql import SparkSession
 from yaml.representer import Representer
-
+from pyspark.sql import Row
+import re
 # Goal : Create a yaml statistic files to do some plot on it
 
 yaml.add_representer(defaultdict, Representer.represent_dict)
 warnings.simplefilter(action='ignore', category=FutureWarning)
-coloredlogs.install(level='DEBUG', fmt='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
+coloredlogs.install(level='INFO', fmt='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
 logger = logging.getLogger(__name__)
+
+class Quad:
+    def __init__(self, s: str, p: str, o: str, g: str):
+        self.s: str = s
+        self.o: str = o
+        self.p: str = p
+        self.g: str = g
+
+    def __str__(self):
+        return ("%s %s %s %s") % (self.s, self.p, self.o, self.g)
+
+    def get_site_of_entity(entity : str):
+        """<http://example.org/s2/MusicAlbum_s0_1749> -> should return <http://example.org/s2>
+            "string_2515" -> should return LITERALS
+        """
+        matched = re.match("<http://example.org/(.+)/.+>", entity)
+        if matched == None:
+            return "LITERALS"
+
+        extract = matched.group(1)
+        return "<http://example.org/%s>" % extract
 
 @click.command()
 @click.argument("input_file")
 @click.argument("output_file")
 
 def compute_statistics(input_file, output_file):
-    df = pd.read_csv(input_file, sep=" ", names=['s', 'p', 'o', 'g','dot'])
-    logger.debug(str(df.head()))
-    logger.debug(str(df[df["p"].str.contains("sameAs")]))
-    set_predicates: set = set(df["p"].apply(lambda p: p.split("/")[3].replace(">","") if "#" not in p else "#type" if "#sameAs" not in p else "#sameAs").unique())
-    logger.debug(set_predicates)
+    spark = SparkSession.builder.master("local").appName("Replicator").config("spark.ui.port", '4050').getOrCreate()
 
-    dict_type = defaultdict(lambda: defaultdict(set))
+    def parseQuads(line):
+        parts = re.split(r'\s+', line)
+        return Quad(parts[0], parts[1], parts[2], parts[3])
 
-    dict_predicate_in = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-    dict_predicate_out = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    lines = spark.read.text(input_file).rdd.map(lambda row: row[0])
+    quads = lines.map(lambda line: parseQuads(line))
 
-    for subj in df['s']:
-        list_subj = str(subj).split('/')
-        logger.debug(
-            f"Site : {list_subj[3]} and type : {str(list_subj[4]).split('_')[0]} for entity : {str(list_subj[4])}")
-        dict_type[str(list_subj[3])][str(list_subj[4].split('_')[0])] |= {str(list_subj[4])}
-
-    logger.debug(dict_type)
-    #exit()
-
-    for index, row in df.iterrows():
-        logger.debug(str(row))
-        for predicate in set_predicates:
-            if predicate in str(row['p']):
-                #logger.debug(f"predicate : {predicate}")
-                #logger.debug(f"p : {str(row['p'])}")
-                site_in = str(row['s']).split('/')[3]
-                site_out_split = str(row['o']).split('/')
-                if len(site_out_split) >= 3 and ('#' not in str(row['o'])):
-                    site_out = site_out_split[3]
-                    dict_predicate_in[str(site_out)][predicate][str(site_in)] += 1
-                    dict_predicate_out[str(site_in)][predicate][str(site_out)] += 1
-                    #logger.debug(f"site_in : {site_in}")
-                    #logger.debug(f"site_out : {site_out}")
-                    #logger.debug(f"predicate : {predicate}")
-
-    #logger.debug(dict_predicate_in)
-    #logger.debug(dict_predicate_out)
-    #exit()
-
-    yaml_dict = defaultdict(lambda: defaultdict(int))
-
-    yaml_dict_properties = defaultdict(
-        lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(int)))))
-
-    for key, val in dict_type.items():
-        for key_t, val_t in val.items():
-            logger.debug(f"Site {key} have {len(val_t)} entity for type {key_t}")
-            yaml_dict[str(key)][str(key_t)] = len(val_t)
-
-    for key, val in dict_type.items():
-        for key_t, val_t in val.items():
-            logger.debug(f"For all site, we have {len(val_t)} entity for type {key_t}")
-            yaml_dict["all_site"][str(key_t)] += len(val_t)
-
-    logger.debug("Merge predicate in out")
-    for key_inner in dict_predicate_in.keys():
-        for key_outtter in dict_predicate_in.keys():
-            for predicate in set_predicates:
-                yaml_dict_properties[key_inner][predicate][key_outtter]["in"] = dict_predicate_in[key_inner][predicate][
-                    key_outtter]
-                yaml_dict_properties[key_inner][predicate][key_outtter]["out"] = \
-                    dict_predicate_out[key_inner][predicate][key_outtter]
+    quads_row=quads.map(lambda x: Row(subject=x.s,predicate=x.p,object=x.o,graph=x.g, origin_subject=Quad.get_site_of_entity(x.s), origin_object=Quad.get_site_of_entity(x.o)))
+    df=spark.createDataFrame(quads_row)
+    df.createOrReplaceTempView("quads")
 
 
-    logger.debug("Merge dict")
-    for k in yaml_dict_properties.keys():
-        yaml_dict[k]["predicates"] = yaml_dict_properties[k]
-        # logger.debug(str(k))
+    entites_per_site = spark.sql("""
+        SELECT graph, object, count(subject) 
+        FROM quads 
+        WHERE predicate ='<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>' 
+        GROUP BY graph, object
+        """)
+
+    entities_all_sites = spark.sql("""
+        SELECT object, count(subject) 
+        FROM quads 
+        WHERE predicate ='<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>' 
+        GROUP BY object
+        """)
+
+    links_out_per_site = spark.sql("""
+        SELECT graph, predicate, origin_subject, origin_object, count(object) 
+        FROM quads 
+        WHERE origin_subject != origin_object AND origin_object!='LITERALS' 
+        GROUP BY graph, predicate, origin_subject, origin_object 
+        """)
+
+    graphs = list(map(lambda x: x[0],spark.sql("""SELECT DISTINCT graph FROM quads""").collect()))
+
+    # messing with data...
+    output_dict = dict()
+    output_dict["perSite"] = dict()
+    for graph in graphs:
+        output_dict["perSite"][graph] = dict()
+
+        # entities per site
+        output_dict["perSite"][graph]["entites_per_site"] = dict()
+        entitesCurrentSite = entites_per_site.rdd.filter(lambda x: x["graph"] == graph).collect()
+        for entity in entitesCurrentSite:
+            output_dict["perSite"][graph]["entites_per_site"][entity[1]] = entity[2]
+
+        # links out per site
+        output_dict["perSite"][graph]["linksOut"] = dict()
+        linksOutCurrentSite = links_out_per_site.rdd.filter(lambda x: x["graph"] == graph).collect()
+        for linkOut in linksOutCurrentSite:
+            if linkOut[1] not in dict(output_dict["perSite"][graph]["linksOut"]).keys():
+                output_dict["perSite"][graph]["linksOut"][linkOut[1]] = dict()
+            output_dict["perSite"][graph]["linksOut"][linkOut[1]][linkOut[3]] = linkOut[4]
+
+    # entities_all_sites
+    output_dict["allSites"] = dict()
+    output_dict["allSites"]["entites_per_site"] = dict()
+    for entity in entities_all_sites.collect():
+        output_dict["allSites"]["entites_per_site"][entity[0]] = entity[1]
+
 
     with open(f"{output_file}", 'w') as yaml_file:
         logger.debug("Writing yaml")
-        yaml.dump(yaml_dict, yaml_file)
-
+        yaml.dump(output_dict, yaml_file)
 
 if __name__ == "__main__":
     compute_statistics()
