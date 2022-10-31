c20822d28727217e437c478248b7e231173b82fa
Mathieu Gicquel <59269440+MathieuGicquel@users.noreply.github.com>
2022-10-31 22:19:42 +0100

    Update graphs_txt_to_nq.py

--- scripts/graphs_txt_to_nq.py
+++ scripts/graphs_txt_to_nq.py
@@ -24,7 +24,7 @@ spark = SparkSession.builder \
     .master("local") \
     .appName("federated-benchmark") \
     .config("spark.ui.port", '4050') \
-    .setMaster("local['*']")
+    .setMaster("local[*]")
 if os.environ["TMPDIR"] is not None:
     print(os.environ["TMPDIR"])
     spark = spark.config("spark.local.dir", os.environ["TMPDIR"])
